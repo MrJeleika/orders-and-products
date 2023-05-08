22e4b2e373de5c51a59c96de38524a9b30ccc2e9
MrJeleika <den932005@gmail.com>
2023-05-09 00:59:42 +0300

    Layout, TopMenu

--- components/NavigationMenu/NavigationMenu.tsx
+++ components/NavigationMenu/NavigationMenu.tsx
@@ -1,3 +1,43 @@
+import Stack from "react-bootstrap/Stack";
+import s from "./NavigationMenu.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Badge from "react-bootstrap/Badge";
+import { GearFill } from "react-bootstrap-icons";
+
 export const NavigationMenu = () => {
-  return <div>NavigationMenu</div>;
+  const router = useRouter();
+  return (
+    <Stack direction="vertical" className={s.menu}>
+      <div className={s.ava}>
+        <div>
+          <Image
+            src={"https://i.pinimg.com/originals/97/e4/2a/97e42a82fc7911961d3ca55f54d1372c.jpg"}
+            alt="Avatar"
+            width={100}
+            height={100}
+          />
+          <Badge bg="light" className={s.badge}>
+            <GearFill fill="gray" />
+          </Badge>
+        </div>
+      </div>
+      <ul className={s.nav}>
+        <li>
+          <Link href={"/"} className={router.pathname == "/" ? s.active : ""}>
+            Приход
+          </Link>
+        </li>
+        <li>
+          <Link href={"/groups"} className={router.pathname == "/groups" ? s.active : ""}>
+            Группы
+          </Link>
+        </li>
+        <li>Продукты</li>
+        <li>Пользователи</li>
+        <li>Настройки</li>
+      </ul>
+    </Stack>
+  );
 };
--- components/TopMenu/TopMenu.tsx
+++ components/TopMenu/TopMenu.tsx
@@ -1,30 +1,52 @@
 import s from "./TopMenu.module.scss";
 import moment from "moment";
-import "moment/locale/he";
+import "moment/locale/ru";
 import "moment-timezone";
-moment().locale("he");
+import { useState } from "react";
+import { Clock } from "react-bootstrap-icons";
+import { Logo } from "components/svg/Logo";
+moment().locale("ru");
+
+interface DateTime {
+  time: string;
+  date: string;
+  day: string;
+}
 
 export const TopMenu = () => {
-  // const tz = moment.tz.guess();
-  // let time: string = moment().tz(tz).format("HH:mm");
-  // let date: string = moment().tz(tz).format("LL");
-
-  // setTimeout(() => {
-  //   time = moment().tz(tz).format("HH:mm");
-  //   date = moment().tz(tz).format("LL");
-  // }, 1000);
+  const tz = moment.tz.guess();
+  const [dateTime, setDateTime] = useState<DateTime>({ time: "", date: "", day: "" });
+  const [date, month, year] = dateTime.date.split(" ");
+  setInterval(() => {
+    setDateTime({
+      time: moment().tz(tz).format("HH:mm"),
+      date: moment().tz(tz).format("LL"),
+      day: moment().tz(tz).format("dddd"),
+    });
+  }, 1000);
 
   return (
     <nav className={s.menu}>
       <div className="d-flex align-items-center">
         <div className={s.menu__logo}>
-          <img src="" alt="Logo" />
+          <Logo color="#88C048" height={50} width={50} />
         </div>
         <h1 className={s.menu__title}>Inventory</h1>
       </div>
       <div>
-        <p>Today</p>
-        <p>{/* {date} {time} */}</p>
+        {date && (
+          <>
+            <p>{dateTime.day}</p>
+            <p>
+              <span className={s.day}>{date} </span>
+              {month}, {year}
+              <span className="mx-2">
+                <Clock fill="#88C048" />
+              </span>
+              {dateTime.time}
+            </p>
+          </>
+        )}
       </div>
     </nav>
   );
