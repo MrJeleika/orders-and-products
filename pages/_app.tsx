b0aae671db7deeb47736145c65246e3414898686
MrJeleika <den932005@gmail.com>
2023-05-10 21:34:55 +0300

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,20 @@ import "../styles/_globals.scss";
 import type { AppProps } from "next/app";
 import { Layout } from "components/Layout";
 import { useRouter } from "next/router";
+import { Provider } from "react-redux";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { emotionCache, pageProps } = props;
   const router = useRouter();
 
   if (router.pathname === "/_error") return <Component {...pageProps} />;
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 export default wrapper.withRedux(App);
